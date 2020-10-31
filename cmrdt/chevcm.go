0be0f6e3fc16d6c155e05395a39780a8e4fe564e
Sasha Avreline <savreline@gmail.com>
2020-10-31 14:09:06 -0700

    Very Basic RPC Working

--- cmrdt/chevcm.go
+++ cmrdt/chevcm.go
@@ -3,22 +3,26 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net"
+	"net/rpc"
 	"os"
 
 	"../util"
 
 	"github.com/DistributedClocks/GoVector/govec"
+	"github.com/DistributedClocks/GoVector/govec/vrpc"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-var db *mongo.Database
-var logger *govec.GoLog
+var no string
 var clPort string
 var srvPort string
-var clConn []*net.UDPConn
-var srvConn net.PacketConn
+var db *mongo.Database
+var client *rpc.Client
+var clLogger *govec.GoLog
+var srvLogger *govec.GoLog
 
 // Record is a DB Record
 type Record struct {
@@ -27,16 +31,15 @@ type Record struct {
 }
 
 func main() {
-	/* Connect to MongoDB, reading port number from the command line */
+	/* Read port numbers from the command line, connect to MongoDB */
 	dbPort := os.Args[1]
 	clPort = os.Args[2]
 	srvPort = os.Args[3]
+	no = os.Args[4]
 	client, ctx := util.Connect(dbPort)
 	db = client.Database("chev")
 	defer client.Disconnect(ctx)
-
-	/* Initialize GoVector logger */
-	logger = govec.InitGoVector("MyProcess", "LogFile", govec.GetDefaultConfig())
+	fmt.Println("STATUS: Connected to DB")
 
 	/* Pre-allocate Keys entry */
 	newRecord := Record{"Keys", []string{}}
@@ -44,28 +47,33 @@ func main() {
 	if err != nil {
 		util.PrintErr(err)
 	}
-	fmt.Println("Pre-allocated keys")
 
 	/* Parse Group Members */
-	ports, err := util.ParseGroupMembers("ports.csv", clPort, srvPort)
-	if err != nil {
-		util.PrintErr(err)
-	}
-	fmt.Println(ports)
+	// ports, err := util.ParseGroupMembers("ports.csv", clPort, srvPort)
+	// if err != nil {
+	// 	util.PrintErr(err)
+	// }
 
 	/* Setup Connection */
-	setupConnection(ports)
+	go rpcserver()
+	go rpcclient()
+	for {
+	}
 
-	// /* Tests */
+	// setupConnection(ports)
+	// fmt.Println(ports)
+	// fmt.Println("STATUS: Parsed Group Membership and Initiated RPC")
+
+	/* Tests */
 	// InsertKey("1")
 	// InsertKey("2")
 	// InsertValue("1", "Hello")
 	// InsertValue("2", "Bye")
 }
 
-/****************************/
-/*** 1: INSERT/REMOVE KEY ***/
-/****************************/
+/**********************/
+/*** 1A: INSERT KEY ***/
+/**********************/
 
 // InsertKey inserts the given key with an empty array for values
 func InsertKey(key string) {
@@ -73,14 +81,9 @@ func InsertKey(key string) {
 	InsertKeyGlobal(key)
 }
 
-// RemoveKey removes the given key
-func RemoveKey(key string) {
-	// TODO
-}
-
 // InsertKeyLocal inserts the key into the local db
 func InsertKeyLocal(key string) {
-	logger.LogLocalEvent("Inserting Key"+key, govec.GetDefaultLogOptions())
+	srvLogger.LogLocalEvent("Inserting Key"+key, govec.GetDefaultLogOptions())
 	filter := bson.D{{Key: "name", Value: "Keys"}}
 	update := bson.D{{Key: "$push", Value: bson.D{
 		{Key: "values", Value: key}}}}
@@ -102,12 +105,17 @@ func InsertKeyLocal(key string) {
 
 // InsertKeyGlobal broadcasts the insertKey operation to other replicas
 func InsertKeyGlobal(key string) {
-
+	var result int
+	err := client.Call("RPCObj.InsertKeyRPC", KeyArgs{key}, &result)
+	if err != nil {
+		util.PrintErr(err)
+	}
+	fmt.Println("Result from RPC", result)
 }
 
-/******************************/
-/*** 2: INSERT/REMOVE VALUE ***/
-/******************************/
+/************************/
+/*** 2A: INSERT VALUE ***/
+/************************/
 
 // InsertValue inserts value into the given key
 func InsertValue(key string, value string) {
@@ -115,14 +123,9 @@ func InsertValue(key string, value string) {
 	InsertValueGlobal(key, value)
 }
 
-// RemoveValue removes value from the given key
-func RemoveValue(key string, value string) {
-	// TODO
-}
-
 // InsertValueLocal inserts the value into the local db
 func InsertValueLocal(key string, value string) {
-	logger.LogLocalEvent("Inserting value"+value, govec.GetDefaultLogOptions())
+	srvLogger.LogLocalEvent("Inserting value"+value, govec.GetDefaultLogOptions())
 	filter := bson.D{{Key: "name", Value: key}}
 	update := bson.D{{Key: "$push", Value: bson.D{
 		{Key: "values", Value: value}}}}
@@ -137,34 +140,81 @@ func InsertValueLocal(key string, value string) {
 
 // InsertValueGlobal broadcasts the insertValue operation to other replicas
 func InsertValueGlobal(key string, value string) {
-
+	var result int
+	err := client.Call("InsertValueRPC", ValueArgs{key, value}, &result)
+	if err != nil {
+		util.PrintErr(err)
+	}
 }
 
-/*************************/
-/*** 3: UDP CONNECTION ***/
-/*************************/
+/**********************/
+/*** 3: RPC METHODS ***/
+/**********************/
 
-// from https://github.com/DistributedClocks/GoVector/blob/master/example/ClientServer/ClientServer.go
-func setupConnection(ports map[string]string) {
-	// set up server
-	// fmt.Println("Listening on server....")
-	// srvConn, err := net.ListenPacket("udp", ":"+srvPort)
-	// if err != nil {
-	// 	util.PrintErr(err)
-	// }
+// RPCObj is the RPC Object
+type RPCObj int
 
-	for aClPort, aSrvPort := range ports {
-		// resolve addresses
-		rAddr, errR := net.ResolveUDPAddr("udp4", ":"+aClPort)
-		fmt.Println(rAddr)
-		util.PrintErr(errR)
-		lAddr, errL := net.ResolveUDPAddr("udp4", ":"+aSrvPort)
-		fmt.Println(lAddr)
-		util.PrintErr(errL)
+// KeyArgs are the arguments to the InsertKeyRPC call
+type KeyArgs struct {
+	key string
+}
 
-		// make connection
-		// aClConn, errDial := net.DialUDP("udp", lAddr, rAddr)
-		// clConn = append(clConn, aClConn)
-		// util.PrintErr(errDial)
+// ValueArgs are the arguments to the InsertValueRPC call
+type ValueArgs struct {
+	key   string
+	value string
+}
+
+// InsertKeyRPC receives incoming insert key call
+func (t *RPCObj) InsertKeyRPC(args *KeyArgs, reply *int) error {
+	fmt.Println("RPC Insert Key")
+	*reply = 100
+	return nil
+}
+
+// InsertValueRPC receives incoming insert value call
+func (t *RPCObj) InsertValueRPC(args *ValueArgs, reply *int) error {
+	fmt.Println("RPC Insert Value")
+	return nil
+}
+
+/*********************/
+/*** 4: RPC SERVER ***/
+/*********************/
+
+func rpcserver() {
+	/* Init RPC */
+	fmt.Println("STATUS: Staring Server")
+	rpcobj := new(RPCObj)
+	server := rpc.NewServer()
+	server.Register(rpcobj)
+	l, e := net.Listen("tcp", ":"+srvPort)
+	if e != nil {
+		log.Fatal("listen error:", e)
 	}
+	fmt.Println("STATUS: Listening on " + srvPort)
+
+	/* Init and Use Vector Clocks */
+	srvLogger = govec.InitGoVector("Server"+no, "LogFile"+no, govec.GetDefaultConfig())
+	options := govec.GetDefaultLogOptions()
+	fmt.Println("STATUS: RPC Ready")
+	vrpc.ServeRPCConn(server, l, srvLogger, options)
+}
+
+/*********************/
+/*** 5: RPC CLIENT ***/
+/*********************/
+
+func rpcclient() {
+	fmt.Println("STATUS: Staring Client")
+	clLogger = govec.InitGoVector("client", "clientlogfile", govec.GetDefaultConfig())
+	options := govec.GetDefaultLogOptions()
+	fmt.Println("STATUS: Client Clocks")
+	var err error
+	client, err = vrpc.RPCDial("tcp", "127.0.0.1:"+clPort, clLogger, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("STATUS: Client Started")
+	InsertKey("1")
 }
