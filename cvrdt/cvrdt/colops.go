8ddea0cc938fd740617e70a5dfd762a5c4b266e8
Sasha Avreline <savreline@gmail.com>
2020-12-01 11:41:28 -0800

    CvRDT: Set Merge Almost Works Properly

--- cvrdt/cvrdt/colops.go
+++ cvrdt/cvrdt/colops.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"../../util"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func mergeCollection(state []util.CvRecord, collection string) {
@@ -36,29 +36,54 @@ func mergeCollection(state []util.CvRecord, collection string) {
 
 // merge positive and negative collections
 func mergeCollections() {
-	/* Iterate over the positive collection */
-	opts := options.Find().SetSort(bson.D{{Key: "name", Value: 1}})
-	cursor, err := db.Collection(posCollection).Find(context.TODO(), bson.D{}, opts)
+	/* Merge the global keys entry */
+	var keysPosRecord, keysNegRecord util.CvRecord
+	filter := bson.D{{Key: "name", Value: "Keys"}}
+	err := db.Collection(posCollection).FindOne(context.TODO(), filter).Decode(&keysPosRecord)
 	if err != nil {
 		util.PrintErr(noStr, err)
 	}
+	err = db.Collection(negCollection).FindOne(context.TODO(), filter).Decode(&keysNegRecord)
+	if err != nil {
+		util.PrintErr(noStr, err)
+	}
+	mergeArray(keysPosRecord.Values, keysNegRecord.Values, "")
 
-	for cursor.Next(context.TODO()) {
+	/* Merge all other entries */
+	// cursor, err := db.Collection(posCollection).Find(context.TODO(), bson.D{})
+	// if err != nil {
+	// 	fmt.Println("error 1")
+	// 	util.PrintErr(noStr, err)
+	// }
+	// for cursor.Next(context.TODO()) {
+	// 	/* Get next item */
+	// 	var posRecord, negRecord util.CvRecord
+	// 	if err := cursor.Decode(&posRecord); err != nil {
+	// 		fmt.Println("error 2")
+	// 		util.PrintErr(noStr, err)
+	// 	}
+
+	// 	/* Look in the negative collection */
+	// 	filter = bson.D{{Key: "name", Value: posRecord.Name}}
+	// 	err = db.Collection(negCollection).FindOne(context.TODO(), filter).Decode(&negRecord)
+	// 	if err != nil {
+	// 		fmt.Println("error 3")
+	// 		util.PrintErr(noStr, err)
+	// 	}
+	// 	mergeArray(posRecord.Values, negRecord.Values, posRecord.Name)
+	// }
+}
+
+// merge specific arrays
+func mergeArray(posArray []util.ValueEntry, negArray []util.ValueEntry, key string) {
+	for _, posRecord := range posArray {
 		/* Since item been in posCollection, default action is to insert it */
 		var insert = true
-
-		/* Get next item */
-		var posRecord util.CvRecord
-		if err := cursor.Decode(&posRecord); err != nil {
-			util.PrintErr(noStr, err)
-		}
+		filter := bson.D{{Key: "name", Value: key}}
 
 		/* Try to locate the item in the negative collection */
-		var negRecord util.CvRecord
-		var filter = bson.D{{Key: "name", Value: posRecord.Name}}
-		err := db.Collection(negCollection).FindOne(context.TODO(), filter).Decode(&negRecord)
-
-		if err == nil {
+		negRecord, found := locateValEntry(negArray, posRecord.Value)
+		if found == true {
 			/* If found, compare time stamps and decide */
 			cmp := posRecord.Timestamp.CompareClocks(negRecord.Timestamp)
 
@@ -74,26 +99,73 @@ func mergeCollections() {
 				}
 			}
 
+			/* Delete positive record */
+			update := bson.D{{Key: "$pull", Value: bson.D{
+				{Key: "values", Value: posRecord}}}}
+			_, err := db.Collection(posCollection).UpdateOne(context.TODO(), filter, update)
+			if err != nil {
+				fmt.Println("error 4")
+				util.PrintErr(noStr, err)
+			}
+
 			/* Delete negative record */
-			_, err = db.Collection(negCollection).DeleteOne(context.TODO(), filter)
+			update = bson.D{{Key: "$pull", Value: bson.D{
+				{Key: "values", Value: negRecord}}}}
+			_, err = db.Collection(negCollection).UpdateOne(context.TODO(), filter, update)
 			if err != nil {
+				fmt.Println("error 5")
+				util.PrintErr(noStr, err)
+			}
+
+			if key == "" {
+				keyFilter := bson.D{{Key: "name", Value: posRecord.Value}}
+
+				/* Delete key in positive collection */
+				_, err := db.Collection(posCollection).DeleteOne(context.TODO(), keyFilter)
+				if err != nil {
+					fmt.Println("error 6")
+					util.PrintErr(noStr, err)
+				}
+
+				/* Delete key in negative collection */
+				_, err = db.Collection(negCollection).DeleteOne(context.TODO(), keyFilter)
+				if err != nil {
+					fmt.Println("error 7")
+					util.PrintErr(noStr, err)
+				}
+			}
+		}
+
+		/* Insert new key, if need be */
+		if insert == true && key == "" {
+			record := util.CmRecord{Name: posRecord.Value, Values: []string{}}
+			_, err := db.Collection("kvs").InsertOne(context.TODO(), record)
+			if err != nil {
+				fmt.Println("error 8")
 				util.PrintErr(noStr, err)
 			}
 		}
 
-		/* Insert if need be */
-		if insert == true {
-			newRecord := util.CmRecord{Name: posRecord.Name, Values: []string{}}
-			_, err := db.Collection("kvs").InsertOne(context.TODO(), newRecord)
+		/* Insert new value, if need be */
+		if insert == true && key != "" {
+			filter = bson.D{{Key: "name", Value: key}}
+			update := bson.D{{Key: "$push", Value: bson.D{
+				{Key: "values", Value: posRecord.Value}}}}
+			_, err := db.Collection("kvs").UpdateOne(context.TODO(), filter, update)
 			if err != nil {
+				fmt.Println("error 9")
 				util.PrintErr(noStr, err)
 			}
-		}
-
-		/* Delete positive record */
-		_, err = db.Collection(posCollection).DeleteOne(context.TODO(), filter)
-		if err != nil {
-			util.PrintErr(noStr, err)
 		}
 	}
+}
+
+// check if the specified value entry in the value entry slice
+func locateValEntry(arr []util.ValueEntry, val string) (util.ValueEntry, bool) {
+	for _, valEntry := range arr {
+		if valEntry.Value == val {
+			return valEntry, true
+		}
+	}
+	return util.ValueEntry{}, false
 }
--- cvrdt/driver/removetest.go
+++ cvrdt/driver/removetest.go
@@ -12,7 +12,7 @@ func removeTest(no int) {
 	conn := util.ConnectDriver(ports[no])
 	var result int
 
-	/* Inserts: insert 2 keys */
+	/* Inserts: insert 2 keys, 2 values for the first key */
 	k := 0
 	for i := 0; i < 2; i++ {
 		key := (no+1)*100 + i
@@ -20,31 +20,37 @@ func removeTest(no int) {
 		if err != nil {
 			util.PrintErr("DRIVER", err)
 		}
-		for j := 0; j < 0; j++ {
-			val := (no+1)*1000 + k
-			err := conn.Call("RPCExt.InsertValue", util.RPCExtArgs{Key: strconv.Itoa(key), Value: strconv.Itoa(val)}, &result)
-			if err != nil {
-				util.PrintErr("DRIVER", err)
+		if i == 0 {
+			for j := 0; j < 2; j++ {
+				val := (no+1)*1000 + k
+				err := conn.Call("RPCExt.InsertValue", util.RPCExtArgs{Key: strconv.Itoa(key), Value: strconv.Itoa(val)}, &result)
+				if err != nil {
+					util.PrintErr("DRIVER", err)
+				}
+				k++
 			}
-			k++
 		}
 	}
 
-	/* Removes: remove 1 key */
+	/* Removes: remove 1 key, 1 value */
 	k = 0
-	for i := 0; i < 1; i++ {
+	for i := 0; i < 2; i++ {
 		key := (no+1)*100 + i
-		err := conn.Call("RPCExt.RemoveKey", util.RPCExtArgs{Key: strconv.Itoa(key)}, &result)
-		if err != nil {
-			util.PrintErr("DRIVER", err)
-		}
-		for j := 0; j < 0; j++ {
-			val := (no+1)*1000 + k
-			err := conn.Call("RPCExt.RemoveValue", util.RPCExtArgs{Key: strconv.Itoa(key), Value: strconv.Itoa(val)}, &result)
+		if i == 1 {
+			err := conn.Call("RPCExt.RemoveKey", util.RPCExtArgs{Key: strconv.Itoa(key)}, &result)
 			if err != nil {
 				util.PrintErr("DRIVER", err)
 			}
-			k++
+		}
+		if i == 0 {
+			for j := 0; j < 1; j++ {
+				val := (no+1)*1000 + k
+				err := conn.Call("RPCExt.RemoveValue", util.RPCExtArgs{Key: strconv.Itoa(key), Value: strconv.Itoa(val)}, &result)
+				if err != nil {
+					util.PrintErr("DRIVER", err)
+				}
+				k++
+			}
 		}
 	}
 
